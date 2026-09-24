733d52165291d9af315d425946d83280d1f83d26
kimkhadija777 <kimkhadija590@gmail.com>
2026-09-24 11:00:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -441,12 +441,12 @@ def welcome_screen():
         with col:
 
             st.markdown(
-                f'''
+                f"""
                 <div class="card">
                     <h3>{title}</h3>
                     <p>{description}</p>
                 </div>
-                ''',
+                """,
                 unsafe_allow_html=True,
             )
 
